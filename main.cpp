cb8d6a442c8e68a011bd0da2c6f5fc41225d3f4b
huyaohua0205@live.com <huyaohua251120>
2019-02-01 13:25:23 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,32 @@
+// consumer main.cpp : 定义控制台应用程序的入口点。
+//
+#include "CKafkaConsumer.h"
+
+static void msg_consume(rd_kafka_message_t *rkmessage, void *opaque)
+{
+    if(rkmessage->err == RD_KAFKA_RESP_ERR_NO_ERROR){
+        printf("[MSG] %d: %s\n", (int)rkmessage->len, (char *)rkmessage->payload);
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    CKafkaConsumer kc;
+
+    char topic[] = "test";
+    char brokers[] = "192.168.8.24:9092";
+    char partitions[] = "0";
+    char groupId[] = "my_group1";
+    consumer_callback consumer_cb = msg_consume; //注册消息回调函数，用户可以自定义此函数
+    void * param_cb = NULL; //param_cb=this;
+    int ret = 0;
+
+    ret = kc.init(topic, brokers, partitions, groupId, consumer_cb, param_cb);
+    if(ret != 0){printf("Error: kc.init(): ret=%d;\n", ret); return 0;}
+
+    ret = kc.getMessage(); //从kafka服务器接收消息
+    if(ret != 0){printf("Error: kc.getMessage(): ret=%d;\n", ret); return 0;}
+
+    return 0;
+}
